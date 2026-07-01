c1323613b55c99caa601c68cea3f46d1c62f7d6e
Vaxa-ar <dosmunsos2015@gmail.com>
2026-07-01 02:22:10 -0300

    Dev-Code-desarrollo-servidor-lineage2

---
+++ scipts.js
@@ -0,0 +1,7 @@
+document.getElementById("contactForm").addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("¡Gracias por tu mensaje! Me pondré en contacto contigo lo antes posible.");
+
+});
